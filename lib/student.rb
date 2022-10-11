0a94ea49b00780100e356bf1a75f757c235f262e
Damla Osman <damlaaosman@gmail.com>
2022-10-11 20:33:49 +1100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,26 @@
-class Student 
+require_relative "user"
 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        super()
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
+
+student = Student.new
+student.learn("hello")
+# puts student.knowledge
+
+# student.learn("space")
+puts student.knowledge
